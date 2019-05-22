8753cedc0b9084417d6373ebc5c9806581618e7f
lykmapipo <lallyelias87@gmail.com>
2019-05-22 17:57:46 +0300

    chore(setup): add peerDependies to external config

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import pkg from './package.json';
 export default [
   {
     input: 'src/index.js',
-    external: [...keys(pkg.dependencies)],
+    external: [...keys(pkg.dependencies), ...keys(pkg.peerDependencies)],
     output: [
       {
         file: pkg.main,
